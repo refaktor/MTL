ec17543c62989466b10b9dc474d77f4144004cfc
Janko M <janko.itm@gmail.com>
2012-02-25 21:54:23 +0100

    improved the coop work koncept, ghost (to), sync from,m ghosts, peers ...

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ CURRDIR=`pwd`
 # commdands.txt to have empty lines
 echo "HI, THIS IS TOTAL WORK IN PROGRESS VERSION!"
 
-for cmd in `cat commands.txt | cut -f 1 | grep -v "^#"`
+for cmd in `cat mtl-commands.txt | cut -f 1 | grep -v "^#"`
 do
     echo "installing $cmd"
     ln -s ${CURRDIR}/mtl-${cmd} /usr/bin/mtl-${cmd}
